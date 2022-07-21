36fbbbddbeaa65cfb123786e0b9ec9bbecb83fbd
domodfg <domodfg@yahoo.com.hk>
2022-07-21 21:31:23 +0800

    Add middle panel to the page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 console.log('checkmate')
 import './style.css';
+
+const container=document.querySelector('#content');
+const middleContainer=document.createElement('div');
+middleContainer.classList.add('middle');
+container.appendChild(middleContainer)
